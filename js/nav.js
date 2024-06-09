242a241534caa1210be02c128271f1c2bf883b9b
leesori1410 <w2319@e-mirim.hs.kr.com>
2024-06-09 18:14:29 +0900

    nav 작업 마무리

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,48 @@
-
 const toggleMenu = () => {
     // HTML 요소 -> js
     const navToggleDiv = document.getElementsByClassName("nav-toggle")[0]; // .nav-toggle 가져오자 
+    const navListUl = document.getElementsByClassName("nav-spread")[0];      // .nav-list 가져오자
     // 선언된 navToggleDiv를 사용
-    const toggleI = navToggleDiv.getElementsByTagName("i")[0];             // .nav-toggle > <i> 가져오자
+    const toggleI = navToggleDiv.getElementsByTagName("i")[0];
+    const navMobile = document.getElementsByClassName("nav-mobile")[0];             // .nav-toggle > <i> 가져오자
 
     // event
     navToggleDiv.onclick = () => {
+        // class에 show-menu를 붙이거나/때자
+        navListUl.classList.toggle("show-menu");
+        navMobile.classList.toggle("show-mobile");
+
         // toggle icon 바꾸자 : bi-list <-> bi-x-lg
         // event .toggle()
         toggleI.classList.toggle("bi-lsit");
         toggleI.classList.toggle("bi-x-lg");
-        // toggleI.classList.add("bi-list");
-        // toggleI.classList.remove("bi-x-lg");
-        // toggleI.classList.add("bi-lg");
-        // toggleI.classList.remove("bi-list");
+        
     }
 
 }
 toggleMenu();
+
+const toggleItems = () => {
+    // 악세사리
+    const containerDiv0 = document.getElementsByClassName("container")[0];
+    const mobileItem0 = document.getElementsByClassName("mobile-item")[0];
+    
+    // 케이크
+    const containerDiv1 = document.getElementsByClassName("container")[1];
+    const mobileItem1 = document.getElementsByClassName("mobile-item")[1];
+
+    // 간식
+    const containerDiv2 = document.getElementsByClassName("container")[2];
+    const mobileItem2 = document.getElementsByClassName("mobile-item")[2];
+
+    mobileItem0.onclick = () => {
+        containerDiv0.classList.toggle("show-mobile");
+    }
+    mobileItem1.onclick = () => {
+        containerDiv1.classList.toggle("show-mobile");
+    }
+    mobileItem2.onclick = () => {
+        containerDiv2.classList.toggle("show-mobile");
+    }
+}
+toggleItems();
