b467ab90f24f2e36235435884d97f42b296e48da
leeseongmi0430 <seongmi987@gmail.com>
2024-06-22 21:08:47 +0900

    json 파일로 데이터 받아와서 HTML 화면 출력(하드코딩 X)

---
+++ js/kategori.js
@@ -0,0 +1,33 @@
+let allData;
+
+// 데이터 보여주기
+const showData = (data) => {
+    const DivContainerSection = document.getElementsByClassName("goods-container")[0];
+
+    let DivString = ""; // 'let' 추가
+    data.forEach(element => {
+        DivString += `<div class="goods">
+                <img src="../image/${element.image}" alt="${element.name}" class="goods-img">
+                <div class="name">${element.name}</div>
+                <div class="hashtags">${element.hashtag}<br>${element.hashtag2}</div>
+            </div>\n`; // 누적 및 줄바꿈 추가
+    });
+    DivContainerSection.innerHTML = DivString; // HTML 문자열을 container에 할당
+};
+
+// 데이터 저장하기
+const setData = (data) => {
+    allData = data;
+    showData(data);
+};
+
+// 데이터 가져오기
+const getData = () => {
+    const url = '../js/data.json';
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => setData(data))
+    .catch((error) => console.log(error));
+};
+
+getData();
